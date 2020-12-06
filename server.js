9de96ef40d1d90c456aaf8d808586062f8ae9062
Kevin Shao <knshao@gmail.com>
2020-12-06 00:06:51 -0800

    Change host and port to use process.env

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(favicon(__dirname + "/public/img/favicon.ico"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.PORT || 3000;
+var server_ip_address = process.env.IP || '127.0.0.1';
 
 http.listen(server_port, server_ip_address, function () {
     console.log("listening on port " + server_port);
